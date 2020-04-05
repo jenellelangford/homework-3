b74550a80d1712527d94002a0164a0d2ad44ce20
Jenelle Langford <jenellelangford@Jenelles-Air.home>
2020-04-05 17:47:25 -0500

    Update.

--- 02-JS-Homework /Develop/script.js
+++ 02-JS-Homework /Develop/script.js
@@ -1,3 +1,4 @@
+// Password variables
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -21,34 +22,33 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// Create functions & loops for password critera
 
 function getPasswordOptions() {
   var passwordLength = parseInt(prompt("How many characters would you like your password to be?"));
-  if (passwordLength < 8 || passwordLength > 128) {
-    alert("Password must be between 8 and 128 characters")
-
+    while (passwordLength < 8 || passwordLength > 128) {
+    alert("Password must be between 8 and 128 characters. Try again.");
+    var passwordLength = parseInt(prompt("How many characters would you like your password to be?"));
   } 
-}
 
-function getPasswordCharacters(){
-var passwordCharacters = prompt("What special characters would you like to use?");
-  if (passwordCharacters === specialCharacters) {
-
+  var passwordCharacters = prompt("What special characters would you like to use?");
+    while (passwordCharacters !== specialCharacters.length) {
+      alert("Invalid answer. Plese try again.");
+      prompt("What special characters would you like to use?");
     }
-}
 
-function getLowerLetters() {
   var lowerCaseLetters = prompt("What lowercase letters would you like to use?");
-    if (lowerCaseLetters === lowerCase) {
-     
+    while (lowerCaseLetters !== lowerCase.length) {
+      alert("Invalid answer. Plese try again.");
+      prompt("What lowercase letters would you like to use?");
     }
-}
 
-function getUpperLetters(){
   var upperCaseLetters = prompt("What uppercase letters would you like to use?");
-  if ( upperCaseLetters === upperCase) {
+    while ( upperCaseLetters !== upperCase.length) {
+      alert("Invalid answer. Plese try again.");
+      prompt("What uppercase letters would you like to use?");
+    }
 
-  }
 }
 
 
@@ -57,8 +57,6 @@ function getUpperLetters(){
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
+// Call out password functions
 getPasswordOptions();
-getPasswordCharacters();
-getLowerLetters();
-getUpperLetters();
+
